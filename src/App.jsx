b7747b201fe3c6316a280676f23046a3ae60091c
Kievv <ievsouza622@gmail.com>
2023-04-30 00:13:31 -0300

    Alterando estrutura de arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Erro404 from './pages/Erro404';
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Route path="*" element={<Erro404 />} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
