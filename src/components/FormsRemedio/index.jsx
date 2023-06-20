99244655ce7c4ae53b2e1de3403014f2269e078b
iev <ievsouza622@gmail.com>
2023-06-19 21:33:56 -0300

    Estrutura de arquivos

--- src/components/FormsRemedio/index.jsx
+++ src/components/FormsRemedio/index.jsx
@@ -5,31 +5,33 @@ import TextField from './TextField';
 const FormsRemedio = () => {
   return (
     <>
-      <form className="formsContainer">
-        <div className="formDiv">
-          <TextField
-            label="MEDICAMENTO"
-            campo="texto"
-            item="medicamento"
-            placeholder="Digite aqui seu medicamento..."
-          />
-          <div className="texto-duplo">
-            <TextField label="DOSAGEM" campo="texto" item="dosagem" placeholder="Quantas cápsulas ao dia..." />
+      <div>
+        <form className="formsContainer flex">
+          <div className="formDiv">
+            <TextField
+              label="MEDICAMENTO"
+              campo="texto"
+              item="medicamento"
+              placeholder="Digite aqui seu medicamento..."
+            />
+            <div className="texto-duplo">
+              <TextField label="DOSAGEM" campo="texto" item="dosagem" placeholder="Quantas cápsulas ao dia..." />
 
-            <TextField label="MILIGRAMAS" campo="texto" item="miligramagem" placeholder="Digite a miligramagem..." />
-          </div>
+              <TextField label="MILIGRAMAS" campo="texto" item="miligramagem" placeholder="Digite a miligramagem..." />
+            </div>
 
-          <div className="texto-triplo">
-            <TextField label="HORÁRIO" campo="texto" item="horario" placeholder="Digite aqui o horário..." />
-            <TextField label="HORÁRIO" campo="texto" item="horario" placeholder="Digite aqui o horário..." />
-            <TextField label="HORÁRIO" campo="texto" item="horario" placeholder="Digite aqui o horário..." />
-          </div>
+            <div className="texto-triplo">
+              <TextField label="HORÁRIO" campo="texto" item="horario" placeholder="Digite aqui o horário..." />
+              <TextField label="HORÁRIO" campo="texto" item="horario" placeholder="Digite aqui o horário..." />
+              <TextField label="HORÁRIO" campo="texto" item="horario" placeholder="Digite aqui o horário..." />
+            </div>
 
-          <div className="btn-holder">
-            <Button botao="CADASTRAR" classe="cadastrar" />
+            <div className="btn-holder">
+              <Button botao="CADASTRAR" classe="cadastrar" />
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 };
