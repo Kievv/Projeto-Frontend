25f70bb7917f71f7949e2f56b73acc742262775c
Kievv <ievsouza622@gmail.com>
2023-06-28 21:21:39 -0300

    links ajeitados

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,38 +1,39 @@
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
 
 export default function Navbar() {
   return (
     <div className={'container'}>
-            <nav className={'nav'}>
-                <Link className={'links'} href="/">REMEDIE</Link>
-                <ul className={'ul'}>
-                    <div className={'botaoNavbar'}>
-                        <li className={'li'}>
-                            <Link href="/">Home</Link>
-                        </li>
-                    </div>
+      <nav className={'nav'}>
+        <Link className={'links'} to="/">
+          REMEDIE
+        </Link>
+        <ul className={'ul'}>
+          <div className={'botaoNavbar'}>
+            <li className={'li'}>
+              <Link to="/">Home</Link>
+            </li>
+          </div>
 
-                    <div className={'botaoNavbar'}>
-                        <li className={'li'}>
-                            <Link href="/consultas">Consultas</Link>
-                        </li>
-                    </div>
+          <div className={'botaoNavbar'}>
+            <li className={'li'}>
+              <Link to="/consultas">Consultas</Link>
+            </li>
+          </div>
 
-                    <div className={'botaoNavbar'}>
-                        <li className={'li'}>
-                            <Link href="/exames">Exames</Link>
-                        </li>
-                    </div>
+          <div className={'botaoNavbar'}>
+            <li className={'li'}>
+              <Link to="/exames">Exames</Link>
+            </li>
+          </div>
 
-                    <div className={'botaoNavbar'}>
-                        <li className={'li'}>
-                            <Link href="/remedios">Remédios</Link>
-                        </li>
-                    </div>
-                </ul>
-            </nav>
-        </div>
-
+          <div className={'botaoNavbar'}>
+            <li className={'li'}>
+              <Link to="/remedios">Remédios</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </div>
   );
 }
